fd5c66b5d3dd5982bf7e036ba4244a0c80c92407
AlexDegerman <alex.degerman.dev@gmail.com>
2024-09-10 01:14:42 +0300

    Add exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,15 +3,17 @@ import Name  from './components/Name'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { content: 'Arto Hellas'}
+    { content: 'Arto Hellas', id: 1, number: "044 500 600"}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       content: newName,
       id: persons.length + 1,
+      number: newNumber
     }
     if (persons.some(e =>  e.content === newName)) (
       alert(`${newName} is already added to phonebook`)
@@ -19,19 +21,22 @@ const App = () => {
     else 
     setPersons(persons.concat(nameObject))
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
